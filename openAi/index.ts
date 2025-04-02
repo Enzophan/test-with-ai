020e3114372b21b73993805b7d6ca71a0080181e
Nhan Phan <32973466+Enzophan@users.noreply.github.com>
2025-04-02 13:42:49 +0700

    Update index.ts

--- openAi/index.ts
+++ openAi/index.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "OPEN_API_KEY",
 });
 
 const completion = openai.chat.completions.create({
